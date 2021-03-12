c56dd33e6a12557e1ee957100a04e47558275fb0
Ridwan Maulana <rdwanf49@gmail.com>
2021-03-12 07:29:45 +0700

    project v1

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+<Header />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Header from './components/Header.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Header
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const colors = require('tailwindcss/colors')
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        gray: colors.coolGray,
+        blue: colors.lightBlue,
+        red: colors.rose,
+        pink: colors.fuchsia,
+      },
+  
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
